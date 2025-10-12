241bb0c84fb62b1d31aa3ce8a29a20a3185822b0
cristmasbox <sonne.zucker@gmx.de>
2025-10-12 18:06:42 +0200

    Methods for converting added

--- glpyhconverter/src/main/java/com/blueapps/glpyhconverter/GlyphConverter.java
+++ glpyhconverter/src/main/java/com/blueapps/glpyhconverter/GlyphConverter.java
@@ -1,4 +1,37 @@
 package com.blueapps.glpyhconverter;
 
 public class GlyphConverter {
+
+    public static String convertToMdC(String glyphX){
+        try {
+            return convertToMdC(convertToXmlDocument(glyphX));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String convertToMdC(Document glyphX){
+        return "";
+    }
+
+    public static String convertToGlyphX(String MdC){
+        return "";
+    }
+
+    public static Document convertToGlyphXDocument(String MdC){
+        try {
+            return convertToXmlDocument(convertToGlyphX(MdC));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Document convertToXmlDocument(String xml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new InputSource(new StringReader(xml)));
+    }
+
 }
