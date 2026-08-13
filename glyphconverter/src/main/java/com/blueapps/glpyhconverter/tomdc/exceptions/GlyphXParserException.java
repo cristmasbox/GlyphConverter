59b0cbf4d774d590e8840142b479438e83ec46f5
cristmasbox <sonne.zucker@gmx.de>
2026-08-13 13:00:53 +0200

    add validation for id in SimpleItem to check for illegal characters

--- glyphconverter/src/main/java/com/blueapps/glpyhconverter/tomdc/exceptions/GlyphXParserException.java
+++ glyphconverter/src/main/java/com/blueapps/glpyhconverter/tomdc/exceptions/GlyphXParserException.java
@@ -3,6 +3,7 @@ package com.blueapps.glpyhconverter.tomdc.exceptions;
 public class GlyphXParserException extends RuntimeException {
     // Exceptions
     public static final String WRONG_ROOT_TAG = "Wrong Root Tag Exception: Root Tag should be \"<%s>\" in stead of \"<%s>\".";
+    public static final String INVALID_ID_CHARACTER = "Invalid Id Character Exception: Id should only contain digits and letters, but found '%c' in id \"%s\".";
 
     public GlyphXParserException(String message) {
         super(message);
--- glyphconverter/src/main/java/com/blueapps/glpyhconverter/tomdc/items/SimpleItem.java
+++ glyphconverter/src/main/java/com/blueapps/glpyhconverter/tomdc/items/SimpleItem.java
@@ -1,11 +1,25 @@
 package com.blueapps.glpyhconverter.tomdc.items;
 
+import com.blueapps.glpyhconverter.tomdc.exceptions.GlyphXParserException;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 public class SimpleItem extends Item {
 
     private String id;
 
     public SimpleItem(String id){
         this.id = id;
+
+        // Check for illegal characters in the id string
+        CharacterIterator it = new StringCharacterIterator(id);
+        while (it.current() != CharacterIterator.DONE) {
+            if (!Character.isDigit(it.current()) && !Character.isAlphabetic(it.current())) {
+                throw new GlyphXParserException(String.format(GlyphXParserException.INVALID_ID_CHARACTER, it.current(), id));
+            }
+            it.next();
+        }
     }
 
     @Override
